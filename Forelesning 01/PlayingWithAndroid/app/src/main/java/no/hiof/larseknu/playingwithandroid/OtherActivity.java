316416ba37fdbd687471d5840bd16632b4ed5870
Lars Emil Knudsen <larsemil87@gmail.com>
2017-08-21 16:37:35 +0200

    Commit of PlayingWithAndroid example

---
+++ Forelesning 01/PlayingWithAndroid/app/src/main/java/no/hiof/larseknu/playingwithandroid/OtherActivity.java
@@ -0,0 +1,19 @@
+package no.hiof.larseknu.playingwithandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+/**
+ * Created by larseknu on 21/08/2017.
+ *
+ */
+
+public class OtherActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
+    }
+}
